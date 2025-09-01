b8e981dc4c86e74d875366d114939d7fd2dec43f
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-02 00:53:05 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import build_database
+def main():
+    # Downloads milex data, build database
+    build_database.main()
+
+    # Generate analysis figures
+    print("Generating figures...")
+    import analysis # jank
+
+    # Compile report
+    print('Compiling report...')
+    filename = '"Arms In and Out of Australia.pdf"'
+    os.system(f"typst compile report.typ {filename}")
+
+    print('Done.')
+
+
+if __name__ == "__main__":
+    main()
