d0097711dd1f1941fc33eaf607bcf6d8cf2ba1a8
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-02 00:55:00 +1000

    Squashed commit of the following:
    commit b8e981dc4c86e74d875366d114939d7fd2dec43f
    Author: George Stuyt <15712782+ogeesan@users.noreply.github.com>
    Date:   Tue Sep 2 00:53:05 2025 +1000
    
        Create main.py
    
    commit cf818d40991e901f09e33417d5944c247ba4961b
    Author: George Stuyt <15712782+ogeesan@users.noreply.github.com>
    Date:   Tue Sep 2 00:52:56 2025 +1000
    
        Fix missing import
    
    commit 6529b88a744cd59376a7a6938168e93b5ffb7f75
    Author: George Stuyt <15712782+ogeesan@users.noreply.github.com>
    Date:   Tue Sep 2 00:27:46 2025 +1000
    
        Create milex data auto-download

--- src/ausarms/datasource.py
+++ src/ausarms/datasource.py
@@ -1,6 +1,8 @@
 """Prepare data obtained from SIPRI and ASPI for SQL database insertion."""
 
 from pathlib import Path
+import urllib
+
 
 import pandas as pd
 
@@ -144,3 +146,12 @@ def normalise_country_name(original: str) -> str:
     if original == "Timor Leste":
         return "Timor-Leste"
     return original
+
+def download_sipri_milex_data() -> None:
+    """Download SIPRI military expenditure data if not already present."""
+    url = 'https://www.sipri.org/sites/default/files/SIPRI-Milex-data-1949-2024_2.xlsx'
+    local_path = Path('data/raw/SIPRI-Milex-data-1949-2024.xlsx')
+    if local_path.exists():
+        return
+
+    urllib.request.urlretrieve(url, local_path)
