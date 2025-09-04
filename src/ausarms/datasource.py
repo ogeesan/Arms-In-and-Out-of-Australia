b1e2ff5681bf3559174122215eac649a140a3a90
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-04 10:05:59 +1000

    Add loading for dec data

--- src/ausarms/datasource.py
+++ src/ausarms/datasource.py
@@ -3,8 +3,8 @@
 from pathlib import Path
 import urllib
 
-
 import pandas as pd
+import yaml
 
 rawdatafolder = Path("data/raw")
 
@@ -155,3 +155,27 @@ def download_sipri_milex_data() -> None:
         return
 
     urllib.request.urlretrieve(url, local_path)
+
+class SafeLineLoader(yaml.SafeLoader):
+    """YAML loader that inserts line number into the extracted data"""
+
+    # augurar's Stack Overflow answer
+    # https://stackoverflow.com/a/53647080
+    def construct_mapping(self, node, deep=False):
+        mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
+        # Add 1 so line numbering starts at 1
+        mapping["__line__"] = node.start_mark.line + 1
+        return mapping
+
+
+def load_multipage_yaml(filepath, with_line_numbers=False):
+    """Loads .yml data with multiple pages."""
+    data = []
+    with open(filepath) as f:
+        if with_line_numbers:
+            stream = yaml.load_all(f, Loader=SafeLineLoader)
+        else:
+            stream = yaml.load_all(f, Loader=yaml.SafeLoader)
+        for document in stream:
+            data.append(document)
+    return data
