32cf67c0e644316620080376d0e745a4c5c9f94b
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-24 13:15:08 +1000

    Create use_project_style()

--- src/ausarms/vis/__init__.py
+++ src/ausarms/vis/__init__.py
@@ -7,3 +7,9 @@ __all__ = [
     "palette",
     "plot"
 ]
+
+def use_project_style() -> None:
+    from pathlib import Path
+    from matplotlib import rc_file
+    filepath = Path(__path__[0]).joinpath('ausarms.mplstyle')
+    rc_file(filepath)
