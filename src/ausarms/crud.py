2ae0eb43be9dfa04fc7c6d8ec76906450fb25274
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-26 16:33:29 +1000

    Refactor sipri data into models/

--- src/ausarms/crud.py
+++ src/ausarms/crud.py
@@ -1,15 +1,24 @@
 from sqlalchemy.orm import Session
 
-from .models import Country, Expenditure, Transfer, Export
+from ausarms.models.sipri import Country, Expenditure, Transfer, Export
+
 
 def get_country(session: Session, name: str) -> Country:
     return session.query(Country).filter_by(name=name).first()
 
-def country_id(session: Session, name: str) -> int:
-    return get_country(session, name).id
 
-def get_military_expenditure(session: Session, country_name: str, year: int, measure_type: str):
+def country_id(session: Session, name: str) -> int:
+    return get_country(session, name).id  # type: ignore
+
+
+def get_military_expenditure(
+    session: Session, country_name: str, year: int, measure_type: str
+):
     country = get_country(session, country_name)
     if not country:
         return None
-    return session.query(Expenditure).filter_by(country=country.id, year=year, measure_type=measure_type).first()
+    return (
+        session.query(Expenditure)
+        .filter_by(country=country.id, year=year, measure_type=measure_type)
+        .first()
+    )
