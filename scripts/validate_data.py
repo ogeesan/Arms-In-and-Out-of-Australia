f52f17fa8fca81507f84f2c17e196cdf38687640
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2026-02-21 15:17:12 +1100

    Improve messaging

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -1,7 +1,6 @@
 """Validate data that has been extracted/transcribed"""
 
 import typer
-import yaml
 
 from ausarms.data import IO, DataPaths
 from ausarms.schemas import dec as schema
@@ -11,14 +10,23 @@ app = typer.Typer()
 
 @app.command()
 def dec():
+    print("Validating Defence Export Controls data...")
     path = DataPaths.dec
 
     filelist = [x for x in path.joinpath("transcribed").glob("*.yml")]
+    failed = []
     for filepath in filelist:
         try:
             schema.FinancialYear.model_validate(IO.load_yaml(filepath))
         except Exception as e:
             print(f"\nError validating {filepath.name}: {e}")
+            failed.append(filepath.name)
+    if failed:
+        print("\nFailed to validate the following files:")
+        for filename in failed:
+            print(f"- {filename}")
+    else:
+        print("All files validated successfully!")
 
 
 @app.command()
